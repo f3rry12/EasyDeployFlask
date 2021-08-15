fe4e3cfc313839ae06488d4fc518675ca6065504
f3rry12 <f3rry12@gmail.com>
2021-08-15 20:24:13 +0700

    Update app.py

--- ap1/app.py
+++ ap1/app.py
@@ -44,7 +44,7 @@ def login():
          hasil.append(v_login)
 
       if hasil:
-        session['name'] = v_login[3]
+        session['mail'] = v_login[1]
         session['name'] = v_login[3]
         return redirect(url_for("bigdataApps"))
         # return redirect('/bigdataApps')
